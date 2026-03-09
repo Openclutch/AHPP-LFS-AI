4bc037d12c18a0f91203421493531ead7079e793
Openclutch <leelandboelsma@gmail.com>
2026-03-09 11:16:26 -0400

    cleaning up

--- SQL Scripts/Reset Database.sql
+++
@@ -1,9 +0,0 @@
-/* RESET DATABASE */
-delete from AttackBattles;
-delete from PracticeTouge;
-delete from CMBattles;
-delete from OwnedCars;
-delete from PlayerSettings;
-delete from Groups where id != 0;
-delete from Players where id != 0;
-/* WARNING!!!!! */
